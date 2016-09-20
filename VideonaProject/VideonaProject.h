0f617bc9af2412c491d7a1a5d5d2020a50e02f34
Alejandro Arjonilla Garcia <aarjonilla@videona.com>
2016-09-20 09:47:01 +0200

    Initial Commit

---
+++ VideonaProject/VideonaProject.h
@@ -0,0 +1,19 @@
+//
+//  VideonaProject.h
+//  VideonaProject
+//
+//  Created by Alejandro Arjonilla Garcia on 20/9/16.
+//  Copyright © 2016 videona. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VideonaProject.
+FOUNDATION_EXPORT double VideonaProjectVersionNumber;
+
+//! Project version string for VideonaProject.
+FOUNDATION_EXPORT const unsigned char VideonaProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VideonaProject/PublicHeader.h>
+
+
